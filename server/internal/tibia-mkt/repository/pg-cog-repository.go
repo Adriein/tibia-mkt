e6336fd7564deac6ff8715df86568487b6a87dea
Adriein <adria.claret@gmail.com>
2024-07-06 17:07:00 +0200

    feat: created new repository for cog table

--- server/internal/tibia-mkt/repository/pg-cog-repository.go
+++ server/internal/tibia-mkt/repository/pg-cog-repository.go
@@ -32,18 +32,6 @@ func (r *PgCogRepository) FindOne(criteria types.Criteria) (types.Cog, error) {
 		}
 	}
 
-	rows, queryErr := r.connection.Query(query)
-
-	defer rows.Close()
-
-	if queryErr != nil {
-		return types.Cog{}, types.ApiError{
-			Msg:      queryErr.Error(),
-			Function: "FindOne -> r.connection.Query()",
-			File:     "pg-cog-repository.go",
-		}
-	}
-
 	var (
 		id         string
 		name       string
@@ -52,45 +40,40 @@ func (r *PgCogRepository) FindOne(criteria types.Criteria) (types.Cog, error) {
 		updated_at string
 	)
 
-	var results []types.Cog
-
-	for rows.Next() {
-		if scanErr := rows.Scan(&id, &name, &link, &created_at, &updated_at); scanErr != nil {
-			return types.Cog{}, types.ApiError{
-				Msg:      scanErr.Error(),
-				Function: "FindOne -> rows.Scan()",
-				File:     "pg-cog-repository.go",
-			}
+	if scanErr := r.connection.QueryRow(query).Scan(&id, &name, &link, &created_at, &updated_at); scanErr != nil {
+		return types.Cog{}, types.ApiError{
+			Msg:      scanErr.Error(),
+			Function: "FindOne -> rows.Scan()",
+			File:     "pg-cog-repository.go",
+			Values:   []string{query},
 		}
-
-		parsedCreatedAt, createdAtParseErr := time.Parse(time.DateTime, created_at)
-
-		if createdAtParseErr != nil {
-			return types.Cog{}, types.ApiError{
-				Msg:      createdAtParseErr.Error(),
-				Function: "FindOne -> time.Parse()",
-				File:     "pg-cog-repository.go",
-			}
-		}
-
-		parsedUpdatedAt, udpatedAtParseErr := time.Parse(time.DateTime, updated_at)
-
-		if udpatedAtParseErr != nil {
-			return types.Cog{}, types.ApiError{
-				Msg:      udpatedAtParseErr.Error(),
-				Function: "FindOne -> time.Parse()",
-				File:     "pg-cog-repository.go",
-			}
-		}
-
-		results = append(results, types.Cog{
-			Id:        id,
-			Name:      name,
-			Link:      link,
-			CreatedAt: parsedCreatedAt,
-			UpdatedAt: parsedUpdatedAt,
-		})
 	}
 
-	return results[0], nil
+	parsedCreatedAt, createdAtParseErr := time.Parse(time.DateTime, created_at)
+
+	if createdAtParseErr != nil {
+		return types.Cog{}, types.ApiError{
+			Msg:      createdAtParseErr.Error(),
+			Function: "FindOne -> time.Parse()",
+			File:     "pg-cog-repository.go",
+		}
+	}
+
+	parsedUpdatedAt, updatedAtParseErr := time.Parse(time.DateTime, updated_at)
+
+	if updatedAtParseErr != nil {
+		return types.Cog{}, types.ApiError{
+			Msg:      updatedAtParseErr.Error(),
+			Function: "FindOne -> time.Parse()",
+			File:     "pg-cog-repository.go",
+		}
+	}
+
+	return types.Cog{
+		Id:        id,
+		Name:      name,
+		Link:      link,
+		CreatedAt: parsedCreatedAt,
+		UpdatedAt: parsedUpdatedAt,
+	}, nil
 }
--- server/pkg/types/error.go
+++ server/pkg/types/error.go
@@ -12,11 +12,16 @@ type ApiError struct {
 	Msg      string
 	Function string
 	File     string
+	Values   []string
 	Domain   bool
 }
 
 func (e ApiError) Error() string {
-	return fmt.Sprintf("Error %s in file %s at function %s", e.Msg, e.File, e.Function)
+	if len(e.Values) > 0 {
+		return fmt.Sprintf("Error %s in file %s calling function %s with values %s", e.Msg, e.File, e.Function, e.Values)
+	}
+
+	return fmt.Sprintf("Error %s in file %s calling function %s", e.Msg, e.File, e.Function)
 }
 
 func (e ApiError) IsDomain() bool {
