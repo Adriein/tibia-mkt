2413a41f7b12248473b7d4b010e359c3c0f39ff5
aclaret <adria.claret@gmail.com>
2025-08-11 16:00:15 +0200

    feat(TM-23) registering new controller

--- server/database/migrations/000005_create_data_snapshot_table.up.sql
+++ server/database/migrations/000005_create_data_snapshot_table.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS data_snapshot_cron (
     executed_by VARCHAR(255) NOT NULL,
     created_at VARCHAR(60) NOT NULL,
     updated_at VARCHAR(60) NOT NULL
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_dsc_cog ON data_snapshot_cron(cog);
 CREATE INDEX IF NOT EXISTS idx_dsc_created_at ON data_snapshot_cron(created_at);
